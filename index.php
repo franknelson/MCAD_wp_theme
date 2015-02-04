9c4410d47be94fd9b537d80f61941aa56a9740fc
franknelson <fnelson12@gmail.com>
2015-02-03 21:18:06 -0600

    initial commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link href='http://fonts.googleapis.com/css?family=Playfair+Display:400,900' rel='stylesheet' type='text/css'/>
+	<title></title>
+
+	<link rel='stylesheet' href='http://frank-nelson.com/base/reset.css' type='text/css'/>
+	<!--<link rel='stylesheet' href='style.css' type='text/css'/> -->
+	<link rel='stylesheet' href='http://frank-nelson.com/wp-content/themes/pressed_static/style.css'/>
+</head>
+	<body>
+		<header>
+			<h1>Pressed &amp Squeezed</h1>
+			<nav>
+				<ul>
+					<li>Home</li>
+					<li>WordPress</li>
+				</ul>
+			</nav>
+		</header>
+		<section>
+			<img src="http://frank-nelson.com/wp-content/themes/pressed_static/images/hero.jpg" />
+
+			<section>
+				<p>Pressed and Squeezed is a full-service WordPress shop. We create custom themes and CMSes for our clients.</p>
+				<p>We also make pancakes on the side.</p>
+			</section>
+		</section>
+		<footer>
+			<h1>Contact Us</h1>
+			<p>Pressed and Squeezed</p>
+			<p>2525 Orange Way, Orchard, CA 91104</p>
+			<p>Phone: (800) 424-3232</p>
+		</footer>
+	</body>
+</html>
