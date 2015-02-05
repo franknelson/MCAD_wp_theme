ec9399007c3611f8b7d560925947442284744d88
franknelson <fnelson12@gmail.com>
2015-02-04 20:45:54 -0600

    adding the php while loop

--- index.php
+++ index.php
@@ -20,12 +20,15 @@
 			</nav>
 		</header>
 		<section>
-			<img src="<?php bloginfo('stylesheet_directory'); ?>images/hero.jpg" />
-
+			<img src="<?php bloginfo('stylesheet_directory'); ?>/images/hero.jpg" />
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 			<section>
 				<p>Pressed and Squeezed is a full-service WordPress shop. We create custom themes and CMSes for our clients.</p>
 				<p>We also make pancakes on the side.</p>
 			</section>
+			<?php enwhile; else :?>
+				<p><?php _e('Sorry, no content'); ?></p>
+			<?php endif; ?>
 		</section>
 		<footer>
 			<h1>Contact Us</h1>
