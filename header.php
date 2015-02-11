f7df6964dbceba7ae9c3a991f4245d53f39ad94a
franknelson <fnelson12@gmail.com>
2015-02-10 22:12:48 -0600

    added header and footer files

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link href='http://fonts.googleapis.com/css?family=Playfair+Display:400,900' rel='stylesheet' type='text/css'/>
+  <link rel='stylesheet' href='http://frank-nelson.com/base/reset.css' type='text/css'/>
+  <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" >
+  <title><?php bloginfo('name'); ?></title>
+  <?php wp_head(); ?>
+</head>
+  <body <?php body_class ( $class ); ?> >
+    <header>
+      <h1>Pressed &amp Squeezed</h1>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>WordPress</li>
+        </ul>
+      </nav>
+    </header>
--- index.php
+++ index.php
@@ -1,24 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<link href='http://fonts.googleapis.com/css?family=Playfair+Display:400,900' rel='stylesheet' type='text/css'/>
-	<title><?php bloginfo('name'); ?></title>
-
-	<link rel='stylesheet' href='http://frank-nelson.com/base/reset.css' type='text/css'/>
-	<!--<link rel='stylesheet' href='style.css' type='text/css'/>
-	<link rel='stylesheet' href='http://frank-nelson.com/wp-content/themes/pressed_static/style.css'/>-->
-	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" >
-</head>
-	<body>
-		<header>
-			<h1>Pressed &amp Squeezed</h1>
-			<nav>
-				<ul>
-					<li>Home</li>
-					<li>WordPress</li>
-				</ul>
-			</nav>
-		</header>
+<?php get_header(); ?>
 		<section>
 			<img src="<?php bloginfo('stylesheet_directory'); ?>/img/hero.jpg" />
 			<?php if ( have_posts() ) :
@@ -35,11 +15,5 @@
 				<p><?php _e('Sorry, no content'); ?></p>
 			<?php endif; ?>
 		</section>
-		<footer>
-			<h1>Contact Us</h1>
-			<p>Pressed and Squeezed</p>
-			<p>2525 Orange Way, Orchard, CA 91104</p>
-			<p>Phone: (800) 424-3232</p>
-		</footer>
-	</body>
-</html>
+
+<?php get_footer(); ?>
