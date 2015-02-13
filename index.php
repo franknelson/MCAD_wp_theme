7b1dece7cc82ceed8146758cc7751f2afcbdbd54
franknelson <fnelson12@gmail.com>
2015-02-12 23:39:37 -0600

    added sidebar and styled it

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <?php get_header(); ?>
-		<section>
-			<img src="<?php bloginfo('stylesheet_directory'); ?>/img/hero.jpg" />
+	<div class="clearfix"	>
+
+		<img src="<?php bloginfo('stylesheet_directory'); ?>/img/hero.jpg" class="clearfix"/>
+
+		<section class="main">
+
 			<?php if ( have_posts() ) :
 					while ( have_posts() ) :
 						the_post(); ?>
+
 			<section>
 				<h1><?php the_title_attribute(); ?></h1>
 				<?php the_content($more_link_text); ?>
@@ -11,9 +16,14 @@
 				This address for this post is: <?php the_permalink(); ?>
 
 			</section>
-		<?php endwhile; else :?>
-				<p><?php _e('Sorry, no content'); ?></p>
-			<?php endif; ?>
+
+				<?php endwhile; else :?>
+					<p><?php _e('Sorry, no content'); ?></p>
+				<?php endif; ?>
+
 		</section>
 
+		<?php get_sidebar(); ?>
+
+	</div>
 <?php get_footer(); ?>
